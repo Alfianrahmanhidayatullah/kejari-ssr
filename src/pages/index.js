c131a61ef82b2dff67eca493f737348382cbc71a
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2025-02-05 02:16:22 +0700

    Fix (ui) : perbaikan berita lainnya

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,14 +11,18 @@ import { Carousel } from "flowbite-react";
 export async function getServerSideProps() {
   try {
     // Fetch data dari backend Laravel
-    const res = await fetch(
-      "https://experimental-clarita-alfianrahman-05697585.koyeb.app/api/berita"
-    );
+    // "https://experimental-clarita-alfianrahman-05697585.koyeb.app/api/berita"
+
+    // const res = await fetch("http://localhost:8000/api/berita");
+    const API_URL =
+      process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api/berita";
+    const res = await fetch(API_URL);
 
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
 
+    // console.log({ res });
     const result = await res.json();
 
     // const resService = await fetch("http://localhost:3000/data/services.json");
@@ -39,8 +43,21 @@ const Home = ({ berita }) => {
   const [show, setShow] = useState(true);
   const [placement, setPlacement] = useState("right");
 
-  console.log("Data Berita:", berita);
+  // console.log("Data Berita:", berita);
+  const [visibleCount, setVisibleCount] = useState(4); // Mulai dengan 4 berita
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLoadMore = () => {
+    setIsLoading(true);
+
+    // Lakukan fetch data atau operasi lainnya
+    setTimeout(() => {
+      setVisibleCount((prevCount) => prevCount + 4); // Tambah 4 berita lagi setiap kali tombol diklik
+
+      setIsLoading(false);
+    }, 1000); // Simulasi waktu loading
+  };
   return (
     <>
       <Header />
@@ -54,7 +71,7 @@ const Home = ({ berita }) => {
               key={item.id}
               className=" container mx-auto flex flex-col-reverse justify-center md:flex-row md:text-left h-screen md:items-center px-4 md:px-0 bg-fixed bg-cover bg-no-repeat xl:h-screen"
               style={{
-                backgroundImage: `url(https://experimental-clarita-alfianrahman-05697585.koyeb.app/public/img/berita/${item.gambar})`,
+                backgroundImage: `url(http://127.0.0.1:8000/img/berita/${item.gambar})`,
               }}
             >
               {/* Overlay Transparan Hitam */}
@@ -165,12 +182,9 @@ const Home = ({ berita }) => {
           <h1 className="text-center font-poppins-bold text-4xl md:text-5xl mb-4">
             Berita Terbaru
           </h1>
-          {/* <h6 className="text-center font-poppins-regular text-base md:text-base tracking-widest">
-            Innovation distinguishes between a leader and a follower.
-          </h6> */}
 
           <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 sm:w-auto">
-            {berita.map((item) => (
+            {berita.slice(0, visibleCount).map((item) => (
               <div
                 key={item.id}
                 className="rounded-xl bg-white shadow-xl border border-[#f5f5f5] sm:h-auto sm:max-h-[1200px] md:h-[70vh] flex flex-col w-[20vw]"
@@ -178,12 +192,12 @@ const Home = ({ berita }) => {
                 <div
                   className="h-full md:h-3/6 bg-cover bg-no-repeat"
                   style={{
-                    backgroundImage: `url(https://experimental-clarita-alfianrahman-05697585.koyeb.app/public/img/berita/${item.gambar})`,
+                    backgroundImage: `url(http://127.0.0.1:8000/img/berita/${item.gambar})`,
                   }}
                 ></div>
                 <div className="px-4 py-3 h-auto">
                   <h4 className="text-base font-bold">{item.judul}</h4>
-                  {/* Membatasi deskripsi hanya 20 kata */}
+                  {/* Membatasi deskripsi hanya 40 kata */}
                   <p className="text-sm mt-2 text-justify">
                     {item.deskripsi.split(" ").slice(0, 40).join(" ")}...
                   </p>
@@ -191,12 +205,47 @@ const Home = ({ berita }) => {
               </div>
             ))}
           </div>
-          <div className="w-full flex justify-center ">
-            <Button
-              classname="rounded-lg bg-[#153164] text-white px-6 py-3 flex gap-2.5 justify-center items-center text-lg font-poppins-bold mt-10 hover:no-underline hover:bg-[#e8bd22]"
-              name="Berita Lainnya"
-            />
-          </div>
+
+          {/* Tampilkan tombol hanya jika masih ada berita yang belum ditampilkan */}
+          {visibleCount < berita.length && (
+            <div className="flex justify-center">
+              <button
+                className={`rounded-lg bg-[#153164] text-white px-6 py-3 flex gap-2.5 justify-center items-center text-lg font-poppins-bold mt-10 hover:no-underline hover:bg-[#17438E] ${
+                  isLoading ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+                onClick={handleLoadMore}
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <div className="flex items-center">
+                    <svg
+                      className="animate-spin h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      />
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      />
+                    </svg>
+                    <span className="ml-2">Loading...</span>
+                  </div>
+                ) : (
+                  <span>Berita Lainnya</span>
+                )}
+              </button>
+            </div>
+          )}
         </div>
       </div>
 
